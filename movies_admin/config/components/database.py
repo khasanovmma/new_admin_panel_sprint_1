b0f6aceec55d73c20b02f302a544b3eddfa1886d
khasanovmma010@gmail.com <khasanovmma010@gmail.com>
2023-08-30 09:47:38 +0500

    refact: update movies admin

--- movies_admin/config/components/database.py
+++ movies_admin/config/components/database.py
@@ -1,7 +1,14 @@
-from django.conf import settings
+from config.settings import env
 
-from split_settings.tools import include
 
-include(
-    "components/database.py",
-)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST", "127.0.0.1"),
+        "PORT": env.int("DB_PORT", 5432),
+        "OPTIONS": {"options": "-c search_path=public,content"},
+    }
+}
